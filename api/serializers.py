d6ff9310a7e548a22dfae9a913c44facf1941ef6
daixiez <71486691+karelbondan@users.noreply.github.com>
2022-06-29 14:55:41 +0700

    fixed improperly configured

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ from .models import Suggestions, Coffee
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
-        fields = ['id', 'username' 'password']
+        fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
     def create(self, validated_data):
